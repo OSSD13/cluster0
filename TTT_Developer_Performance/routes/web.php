78cbcab7f321605a0f09a904075fdf6966b3fb0d
MERCURY <122726524+MercuRy47@users.noreply.github.com>
2025-04-03 21:47:13 +0700

    feat(layout): สร้าง layout สำหรับ tester

--- TTT_Developer_Performance/routes/web.php
+++ TTT_Developer_Performance/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReportController;
 
 // Login
 Route::get('/', [LoginController::class, 'index'])->name('login');
@@ -41,3 +42,6 @@ Route::prefix('dev')->group(function () {
 Route::prefix('tester')->group(function () {
 
 });
+
+// Report
+Route::get('/report', [ReportController::class, 'index']);
