b707645a3e5034e2de7b5bb00bcca1b2c69cb16f
Thanusin leenarat <164463161+lilsunnoi@users.noreply.github.com>
2025-04-04 23:33:56 +0700

    add view addbacklog

--- TTT_Developer_Performance/routes/web.php
+++ TTT_Developer_Performance/routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MinorcaseController;
 use App\Http\Controllers\CreateMinorcaseController;
+use App\Http\Controllers\BacklogController;
+use App\Http\Controllers\CreateBacklogController;
 
 // Login
 Route::get('/', [LoginController::class, 'index'])->name('login');
@@ -54,4 +56,7 @@ Route::get('/changepassword', [ProfileController::class,'changePassword']);
 
 Route::get('/minorcase', [MinorcaseController::class,'index']);
 Route::get('/addminorcase', [CreateMinorcaseController::class,'index']);
+
+Route::get('backlog', [BacklogController::class,'index']);
+Route::get('/addBacklog', [CreateBacklogController::class,'index']);
 
