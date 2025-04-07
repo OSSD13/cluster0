8bc8fb4b7a1f2a77ebe0a0b9756feac1ab724419
Jfyy-x <fasaiswy502@gmail.com>
2025-04-07 09:52:58 +0700

    update path view card

--- TTT_Developer_Performance/app/Http/Controllers/TeamPerformanceController.php
+++ TTT_Developer_Performance/app/Http/Controllers/TeamPerformanceController.php
@@ -11,20 +11,6 @@ class TeamPerformanceController extends Controller
 {
     public function card(){
         $cards = Card::All();
-        return view('pages.teamPerformance',compact('cards'));
+        return view('pages/dashboard/teamPerformance',compact('cards'));
     }
-
-    public function insertCard(){
-        Card::create([
-            'crd_trc_id' => 1,
-            'crd_boardname' => 'Board1',
-            'crd_listname' => 'List1',
-            'crd_title' => 'Task Title',
-            'crd_detail' => 'Detail of task',
-            'crd_member_fullname' => 'nah',
-            'crd_point' => 10
-        ]);
-        return redirect('pages.teamPerformance');
-    // return redirect('/card');
-}
 }
